191fffeabf1cd65d735fe23b6df3e979b9583fbc
Julio Pinheiro <juliorenan@live.com>
2021-04-12 22:20:43 -0300

    fix: adicionado modo correto de pegar as divs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const puppeteer = require('puppeteer');
 
-const site = 'https://www.shoptime.com.br/produto/2056887915/placa-de-video-asus-geforce-gtx-1660-super-oc-edition?opn=GOOGLEXML&sellerid=18552346000168';
+const site = 'https://www.submarino.com.br/produto/2818205379/placa-de-video-gigabyte-amd-radeon-rx-5600-xt-windforce-oc-6g-6gb-gddr6-rev-1-0-gv-r56xtwf2oc-6gd';
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -21,10 +21,16 @@ server.listen(port, hostname, () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(site);
-    // await page.screenshot({ path: 'example2.png' });
-    page.on('console', (log) => console[log._type](log._text));
-  
-    
+    await page.setDefaultNavigationTimeout(60000);
+    await page.screenshot({ path: 'example4.png' });
+
+    try {
+      const element = await page.$("#indisponivel");
+      const text = await page.evaluate(element => element.textContent, element);
+      console.log(text);  
+    } catch (error) {
+      console.log(error);
+    }
 
     await browser.close();
   })();
