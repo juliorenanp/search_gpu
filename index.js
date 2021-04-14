743fe1059dd32eda3a12d0d15140f711ded179dd
Julio Pinheiro <juliorenan@live.com>
2021-04-13 22:08:58 -0300

    fix: it worked, show!

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ server.listen(port, hostname, () => {
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     await page.goto(site);
     await page.setDefaultNavigationTimeout(60000);
-    await page.screenshot({ path: 'example4.png' });
+    await page.screenshot({ path: 'ronaldo.png' });
 
     try {
-      const element = await page.$("#indisponivel");
+      const element = await page.$("#title-stock");
       const text = await page.evaluate(element => element.textContent, element);
       console.log(text);  
     } catch (error) {
